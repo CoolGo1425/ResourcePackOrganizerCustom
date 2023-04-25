c1a6a06417f1ce08e88b9fd6adcf2f56ca3f712f
CoolGo1425 <93942561+CoolGo1425@users.noreply.github.com>
2023-04-26 02:02:23 +0300

    Initial commit

---
+++ src/main/java/net/dodi/rpo/ResourcePackOrganizerCustom.java
@@ -0,0 +1,15 @@
+package net.dodi.rpo;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ResourcePackOrganizerCustom implements ModInitializer {
+	public static final String MOD_ID = "rpo";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
